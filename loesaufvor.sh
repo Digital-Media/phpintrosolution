c78eaf570c7574e2e29384f5a52541f4aa3532c0
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-01-17 09:18:26 +0100

    Linux-LF for shell scripts

--- loesaufvor.sh
+++ loesaufvor.sh
@@ -18,30 +18,13 @@
 # that is done for every file in the list below
 cd "/var/www/html/code/wbt2ue"
 #
-sed "s/\/\*##/\/\/##/g" index.php > /tmp/index.1
-sed "s/\/\/--/\/\*--/g" /tmp/index.1 > index.php
+sed "s/\/\*##/\/\/##/g" contact.php > /tmp/contact.1
+sed "s/\/\/--/\/\*--/g" /tmp/contact.1 > contact.php
 #
 sed "s/\/\*##/\/\/##/g" login.php > /tmp/login.1
 sed "s/\/\/--/\/\*--/g" /tmp/login.1 > login.php
 #
 sed "s/\/\*##/\/\/##/g" register.php > /tmp/register.1
 sed "s/\/\/--/\/\*--/g" /tmp/register.1 > register.php
-#
-sed "s/\/\*##/\/\/##/g" includes/ImageProcessing.php > /tmp/image.1
-sed "s/\/\/--/\/\*--/g" /tmp/image.1 > includes/ImageProcessing.php
-#
-cd "/var/www/html/code/wbt2ue"
-#
-sed "s/\/\*##/\/\/##/g" index.php > /tmp/index.1
-sed "s/\/\/--/\/\*--/g" /tmp/index.1 > index.php
-#
-sed "s/\/\*##/\/\/##/g" includes/FileAccess.php > /tmp/file.1
-sed "s/\/\/--/\/\*--/g" /tmp/file.1 > includes/FileAccess.php
-#
-sed "s/\/\*##/\/\/##/g" js/addressbook.js > /tmp/ab.1
-sed "s/\/\/--/\/\*--/g" /tmp/ab.1 > js/addressbook.js
-#
-sed "s/\/\*##/\/\/##/g" js/searchsuggest.js > /tmp/search.1
-sed "s/\/\/--/\/\*--/g" /tmp/search.1 > js/searchsuggest.js
 # delete unnecessary files
 rm /tmp/*.1
