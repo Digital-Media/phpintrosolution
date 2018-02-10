d0ce7857bb30b4e41d4680da5e35eee3d63ce305
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-02-10 21:45:30 +0100

    moving exercises to src/exercises

--- loesaufvor.sh
+++ loesaufvor.sh
@@ -16,7 +16,7 @@
 # // end exercise */      # line comment is active until sed writes starting /*, this line must not be changed
 # set -x
 # that is done for every file in the list below
-cd "/var/www/html/code/phpintro"
+cd "/var/www/html/code/phpintro/src/exercises/templates"
 #
 sed "s/\/\*##/\/\/##/g" contact.php > /tmp/contact.1
 sed "s/\/\/--/\/\*--/g" /tmp/contact.1 > contact.php
@@ -24,8 +24,12 @@ sed "s/\/\/--/\/\*--/g" /tmp/contact.1 > contact.php
 sed "s/\/\*##/\/\/##/g" imprint.php > /tmp/imprint.1
 sed "s/\/\/--/\/\*--/g" /tmp/imprint.1 > imprint.php
 #
+cd "/var/www/html/code/phpintro/src/exercises/login"
+#
 sed "s/\/\*##/\/\/##/g" login.php > /tmp/login.1
 sed "s/\/\/--/\/\*--/g" /tmp/login.1 > login.php
+#
+cd "/var/www/html/code/phpintro/src/exercises/register"
 #
 sed "s/\/\*##/\/\/##/g" register.php > /tmp/register.1
 sed "s/\/\/--/\/\*--/g" /tmp/register.1 > register.php
