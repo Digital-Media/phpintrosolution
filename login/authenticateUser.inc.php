b1f545a5c5cdb1cb16c95fc4c17cd8189e14fc7e
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-02-13 16:05:49 +0100

    moving exercies/templates to excercise/using-templates

--- login/authenticateUser.inc.php
+++ login/authenticateUser.inc.php
@@ -1,5 +1,5 @@
 <?php
-$users = $this->fileAccess->loadContents(USER_DATA_PATH);
+$users = $this->fileAccess->loadContents(self::USER_DATA_PATH);
 
 foreach ($users as $user) {
     if ($user[self::USERNAME] === $_POST[self::USERNAME] && password_verify($_POST[self::PASSWORD], $user[self::PASSWORD])) {
