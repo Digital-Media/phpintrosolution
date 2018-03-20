828f9c83b4955174984d6665510ec4fce3f40f66
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-03-20 15:41:41 +0100

    improved TODOs

--- oophp/ShowFormInput.php
+++ oophp/ShowFormInput.php
@@ -42,11 +42,11 @@ class ShowFormInput
         // TODO Im Konstrutor
         // TODO Geben sie die oben definierte Klassenkonstante hier aus.
         // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
-        // todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+        // TODO Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
         echo "<p>CLASS_CONST: " . self::CLASS_CONST . " </p>";
         // TODO Geben sie die oben definierte Klasseneigenschaft hier aus.
         // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
-        // todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+        // TODO Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
         echo "<p>\$myPublicVar: " . $this->myPublicVar . " </p>";
         // TODO Geben Sie den Wert der globalen Konstante DEBUG aus error_handling.php hier aus
         // TODO Verfahren sie dazu wie bei der Klassenkonstante
@@ -99,7 +99,7 @@ var_dump($showFormInput);
 
 // TODO Geben sie die oben definierte Klasseneigenschaft hier aus.
 // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
-// todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+// TODO Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
 echo "<p>\$myPublicVar: " . $showFormInput2->myPublicVar . " </p>";
 
 // TODO Geben Sie den Wert der globalen Konstante DEBUG aus error_handling.php hier aus
