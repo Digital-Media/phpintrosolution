cd1439339b293a287a60ea9805d914f781ad1ee3
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-03-23 13:19:03 +0100

    providing more solutions in different difficulties

--- regex/moneycounter.php
+++ regex/moneycounter.php
@@ -10,10 +10,15 @@
             <h2 >Counting Results</h2>
             <?php
             $input = $_POST["input"];
-            preg_match_all("/((^[1-9]| [1-9]|(\R[1-9]))([0-9]*)| 0)(,[0-9]{2}|,-|)( EUR\h| EUR\b)/", $input, $matches);
+            // teststring.txt, no $matches[1] without " EUR"
+            // preg_match_all("/( [1-9])([0-9]*)(,[0-9]{2}|,-|)( EUR\b)/", $input, $matches);
+            // teststring.txt, $matches[1] contains only decimals additional () around decimals
+            // preg_match_all("/(( [1-9])([0-9]*)(,[0-9]{2}|,-|))( EUR\b)/", $input, $matches);
+            preg_match_all("/(( [1-9]{1}|^[1-9]|\R[1-9]| 0[^0-9]|^0|\R0[^0-9])([0-9]*)(,[0-9]{2}|,-|))( EUR\b)/", $input, $matches); // challange.txt
 
             $sum = 0;
             //print_r($matches);
+            //*// complicated version with $matches containing valid amounts with decimals and " EUR"
             foreach ($matches[0] as $money) {
                 // $amount = str_replace(",", ".", $money);
 
@@ -35,8 +40,16 @@
 
                 $sum += $amount;
             }
+            //*/
+            /*// simple version with $matches[1] containing all valid decimals without " EUR". Additional () around the decimals are necessary in regex
+            foreach ($matches[1] as $money) {
+                $amount[]= str_replace(",", ".", $money);
+            }
+            $sum = array_sum($amount);
+            //*/
 
-            echo "<p>Original text was: <blockquote>" . htmlentities($input) . "</blockquote></p>", PHP_EOL;
+
+            echo "<p>Original text was: <blockquote>" . nl2br(htmlentities($input)) . "</blockquote></p>", PHP_EOL;
             echo "<p>The total amount is: <strong>" . number_format($sum, 2, ',', '.') . " EUR</strong></p>", PHP_EOL;
             ?>
             <a href="index.html">Start Over</a>
