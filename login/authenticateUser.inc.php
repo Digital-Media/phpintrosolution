aad63dd55632966a13b6380a1c9ce2e485ab3834
Martin Harrer <martin.harrer@fh-hagenberg.at>
2019-09-25 16:14:38 +0200

    added argon2i

--- login/authenticateUser.inc.php
+++ login/authenticateUser.inc.php
@@ -1,8 +1,12 @@
-$users = $this->fileAccess->loadContents(self::USER_DATA_PATH);
+        $users = $this->fileAccess->loadContents(self::USER_DATA_PATH);
 
-foreach ($users as $user) {
-    if ($user[self::USERNAME] === $_POST[self::USERNAME] && password_verify($_POST[self::PASSWORD], $user[self::PASSWORD])) {
-        return true;
-    }
-}
-return false;
+        foreach ($users as $key => $user) {
+            if ($user[self::USERNAME] === $_POST[self::USERNAME] && password_verify($_POST[self::PASSWORD], $user[self::PASSWORD])) {
+                if (password_needs_rehash($user[self::PASSWORD], PASSWORD_ARGON2I)) {
+                    $users[$key][self::PASSWORD] = password_hash($_POST['password'], PASSWORD_ARGON2I);
+                    $this->fileAccess->storeContents(self::USER_DATA_PATH, $users);
+                }
+                return true;
+            }
+        }
+        return false;
